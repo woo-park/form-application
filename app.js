a242c8c95da655743de96abeb72d5544faba5d62
Woo <leo9634@naver.com>
2020-07-30 21:29:24 -0400

    changing to ejs

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
-const cookieParser = require('express-session');
-const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
 const flash = require('connect-flash');
 const passport = require('passport');
+
 require('dotenv').config();
 
 const indexRouter = require('./routes/index');
@@ -12,22 +13,26 @@ const authRouter = require('./routes/auth');
 const { sequelize } = require('./models');
 const passportConfig = require('./passport');
 
+const expressLayouts = require('express-ejs-layouts');
+
 const app = express();
 sequelize.sync();
 passportConfig(passport);
 
-const sessionMiddleware = session({
-  saveUninitialized: false,
-  resave: false,
-  secret: process.env.COOKIE_SECRET,
-  cookie: {
-    httpOnly: true,
-    secure: false,
-  },
-});
+// const sessionMiddleware = session({
+//   saveUninitialized: false,
+//   resave: false,
+//   secret: process.env.COOKIE_SECRET,
+//   cookie: {
+//     httpOnly: true,
+//     secure: false,
+//   },
+// })
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
 app.set('port', process.env.PORT || 8010);
 
 app.use(morgan('dev'));
@@ -35,8 +40,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/img', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(sessionMiddleware);
+app.use(expressSession({
+  saveUninitialized: false,
+  resave: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+}));
 app.use(passport.initialize());
 app.use(flash());
 
